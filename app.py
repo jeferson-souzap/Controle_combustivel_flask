b86338391509e9a8a5338b479d039ca200df2c0f
Jeferson Souza <mp_bra_nco@hotmail.com>
2025-09-05 23:42:17 -0300

    Layout funcinando e links das paginas funcionando
    Finalizado o link e rotas das paginas e começando dar função e salvamento de banco de dados.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from blueprints.cadastro import cadastros_bp
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+
+from database.cadastro_combustivel import Cadastrar_combustivel_db
 
 app = Flask(__name__)
 
@@ -20,14 +22,40 @@ def cadastro_marcas():
 def cadastro_modelos():
     return render_template("cadastro_modelos.html")
 
-@app.route("/cadastro_combustivel")
+
+
+@app.route("/cadastro_combustivel", methods=["GET", "POST"])
 def cadastro_combustivel():
-    return render_template("cadastro_combustivel.html")
+    if request.method == "GET":
+        return render_template("cadastro_combustivel.html")
+    
+    if request.method == "POST":
+        nomeCombustivel = request.form["nomeCombustivel"]
+        precoCombustivel = request.form['precoCombustivel']
+
+        sucesso, msg = Cadastrar_combustivel_db(nomeCombustivel, precoCombustivel)
+
+        if sucesso:
+            flash(msg, "success")
+        else:
+            flash(msg, "danger")
+
+        return redirect(url_for("cadastro_combustivel"))
 
 
 @app.route('/cadastro_veiculos')
 def cadastro_veiculos():
     return render_template("cadastro_veiculos.html")
+
+
+@app.route('/cadastro_motorista')
+def cadastro_motorista():
+    return render_template("cadastro_motorista.html")
+
+@app.route('/movimentacoes')
+def movimentacoes():
+    return render_template("movimentacoes.html")
+
 
 
 if __name__ == "__main__":
--- blueprints/cadastro.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-
-
-cadastros_bp = Blueprint("cadastro", __name__, template_folder="../templates")
-
-@cadastros_bp.route("/")
-def index():    
-    return render_template("cadastro.html")
